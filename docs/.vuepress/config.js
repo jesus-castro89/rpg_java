0cb098a10b3762318de438274c28a25af761355d
Jesús Aurelio Castro Magaña <jesus_castrom@my.unitec.edu.mx>
2024-02-05 01:03:34 -0600

    Deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,6 @@ import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
-  base: "/rpg_java/",
   bundler: viteBundler(),
   theme: defaultTheme(),
 })
