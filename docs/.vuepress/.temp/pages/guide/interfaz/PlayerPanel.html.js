d05b41ac6f086f053befc532bee9f12fd17f074e
Jesús Aurelio Castro Magaña <jesus_castrom@my.unitec.edu.mx>
2024-04-17 03:16:36 -0600

    4. Que empiece la acción

---
+++ docs/.vuepress/.temp/pages/guide/interfaz/PlayerPanel.html.js
@@ -0,0 +1,16 @@
+import comp from "C:/xampp/node/rpg_java/docs/.vuepress/.temp/pages/guide/interfaz/PlayerPanel.html.vue"
+const data = JSON.parse("{\"path\":\"/guide/interfaz/PlayerPanel.html\",\"title\":\"El panel del jugador\",\"lang\":\"es-ES\",\"frontmatter\":{\"icon\":\"gamepad\",\"title\":\"El panel del jugador\",\"description\":\"El panel del jugador es una interfaz que muestra información sobre el jugador y su estado actual.\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://jesus-castro89.github.io/rpg_java/rpg_java/guide/interfaz/PlayerPanel.html\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"RPG Java\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"El panel del jugador\"}],[\"meta\",{\"property\":\"og:description\",\"content\":\"El panel del jugador es una interfaz que muestra información sobre el jugador y su estado actual.\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"article\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"es-ES\"}],[\"meta\",{\"property\":\"article:author\",\"content\":\"M. en E. Jesús Aurelio Castro Magaña\"}],[\"script\",{\"type\":\"application/ld+json\"},\"{\\\"@context\\\":\\\"https://schema.org\\\",\\\"@type\\\":\\\"Article\\\",\\\"headline\\\":\\\"El panel del jugador\\\",\\\"image\\\":[\\\"\\\"],\\\"dateModified\\\":null,\\\"author\\\":[{\\\"@type\\\":\\\"Person\\\",\\\"name\\\":\\\"M. en E. Jesús Aurelio Castro Magaña\\\",\\\"url\\\":\\\"https://github.com/jesus-castro89\\\"}]}\"]]},\"headers\":[{\"level\":2,\"title\":\"Implementación de la clase PlayerPanel\",\"slug\":\"implementacion-de-la-clase-playerpanel\",\"link\":\"#implementacion-de-la-clase-playerpanel\",\"children\":[]},{\"level\":2,\"title\":\"Implementación de la clase PortraitLabel\",\"slug\":\"implementacion-de-la-clase-portraitlabel\",\"link\":\"#implementacion-de-la-clase-portraitlabel\",\"children\":[]},{\"level\":2,\"title\":\"Implementación de la clase HpLabel\",\"slug\":\"implementacion-de-la-clase-hplabel\",\"link\":\"#implementacion-de-la-clase-hplabel\",\"children\":[]},{\"level\":2,\"title\":\"Implementación de la clase MpLabel\",\"slug\":\"implementacion-de-la-clase-mplabel\",\"link\":\"#implementacion-de-la-clase-mplabel\",\"children\":[]},{\"level\":2,\"title\":\"Implementación de la clase RedTextLabel\",\"slug\":\"implementacion-de-la-clase-redtextlabel\",\"link\":\"#implementacion-de-la-clase-redtextlabel\",\"children\":[]}],\"readingTime\":{\"minutes\":5.65,\"words\":1695},\"filePathRelative\":\"guide/interfaz/PlayerPanel.md\"}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
---
+++ docs/.vuepress/.temp/pages/guide/interfaz/enemy_player.html.js
@@ -0,0 +1,16 @@
+import comp from "C:/xampp/node/rpg_java/docs/.vuepress/.temp/pages/guide/interfaz/enemy_player.html.vue"
+const data = JSON.parse("{\"path\":\"/guide/interfaz/enemy_player.html\",\"title\":\"El enemigo y el jugador\",\"lang\":\"es-ES\",\"frontmatter\":{\"icon\":\"gamepad\",\"title\":\"El enemigo y el jugador\",\"description\":\"Una actualización necesaria.\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://jesus-castro89.github.io/rpg_java/rpg_java/guide/interfaz/enemy_player.html\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"RPG Java\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"El enemigo y el jugador\"}],[\"meta\",{\"property\":\"og:description\",\"content\":\"Una actualización necesaria.\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"article\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"es-ES\"}],[\"meta\",{\"property\":\"article:author\",\"content\":\"M. en E. Jesús Aurelio Castro Magaña\"}],[\"script\",{\"type\":\"application/ld+json\"},\"{\\\"@context\\\":\\\"https://schema.org\\\",\\\"@type\\\":\\\"Article\\\",\\\"headline\\\":\\\"El enemigo y el jugador\\\",\\\"image\\\":[\\\"\\\"],\\\"dateModified\\\":null,\\\"author\\\":[{\\\"@type\\\":\\\"Person\\\",\\\"name\\\":\\\"M. en E. Jesús Aurelio Castro Magaña\\\",\\\"url\\\":\\\"https://github.com/jesus-castro89\\\"}]}\"]]},\"headers\":[{\"level\":2,\"title\":\"El enemigo\",\"slug\":\"el-enemigo\",\"link\":\"#el-enemigo\",\"children\":[]},{\"level\":2,\"title\":\"El jugador\",\"slug\":\"el-jugador\",\"link\":\"#el-jugador\",\"children\":[]},{\"level\":2,\"title\":\"Los tipos de armadura del jugador\",\"slug\":\"los-tipos-de-armadura-del-jugador\",\"link\":\"#los-tipos-de-armadura-del-jugador\",\"children\":[]},{\"level\":2,\"title\":\"Los skills del jugador\",\"slug\":\"los-skills-del-jugador\",\"link\":\"#los-skills-del-jugador\",\"children\":[]},{\"level\":2,\"title\":\"La skill de curación básica\",\"slug\":\"la-skill-de-curacion-basica\",\"link\":\"#la-skill-de-curacion-basica\",\"children\":[]},{\"level\":2,\"title\":\"La skill de ataque furioso\",\"slug\":\"la-skill-de-ataque-furioso\",\"link\":\"#la-skill-de-ataque-furioso\",\"children\":[]},{\"level\":2,\"title\":\"Conclusión\",\"slug\":\"conclusion\",\"link\":\"#conclusion\",\"children\":[]}],\"readingTime\":{\"minutes\":9.25,\"words\":2776},\"filePathRelative\":\"guide/interfaz/enemy_player.md\"}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
